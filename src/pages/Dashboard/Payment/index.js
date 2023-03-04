f390347fb6bcde4f195b2ec9ea99a88a9094b87e
ConstantinoRafael <rafael.constantino89@gmail.com>
2023-03-03 22:05:11 -0300

    feat: payment page with ticketTypes and select

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,61 +1,8 @@
-import styled from 'styled-components';
+import TicketTypes from '../../../components/Dashboard/Payment';
+import useEnrollment from '../../../hooks/api/useEnrollment';
 
 export default function Payment() {
-  return (
-    <DashbordaArea>
-      <TitlePage>Ingresso e pagamento</TitlePage>
-      <Subtitle>Primeiro, escolha sua modalidade de ingresso</Subtitle>
-      <Choices>
-        <Container>
-          <h2>Presencial</h2>
-          <p>R$ 250</p>
-        </Container>
-        <Container>
-          <h2>Online</h2>
-          <p>R$ 100</p>
-        </Container>
-      </Choices>
-    </DashbordaArea>
-  );
+  const { enrollment } = useEnrollment();
+
+  return enrollment ? <TicketTypes /> : '';
 }
-
-const DashbordaArea= styled.div`
-  font-family: 'Roboto', sans-serif;
-`;
-
-const TitlePage = styled.h1`
-  font-size: 34px;
-`;
-
-const Subtitle = styled.p`
-  margin-top: 35px;
-  font-size: 20px;
-  color: #8e8e8e;
-`;
-
-const Choices = styled.div`
-  display: flex;
-`;
-const Container = styled.div`
-  width: 145px;
-  height: 145px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin-top: 15px;
-  margin-right: 20px;
-
-  border: 1px solid #cecece;
-  border-radius: 20px;
-
-  h2 {
-    font-size: 16px;
-    color: #454545;
-  }
-
-  p {
-    font-size: 14px;
-    color: #898989;
-  }
-`;
