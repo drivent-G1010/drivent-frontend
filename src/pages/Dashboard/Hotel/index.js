028aed2a9583e035a60fef3ac9ef3f2e19f34007
ConstantinoRafael <rafael.constantino89@gmail.com>
2023-03-07 16:35:40 -0300

    feat: not incluse hotel ticke type (hotel page)

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,3 +1,18 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+import NotIncludesHotel from '../../../components/Dashboard/NotIncludesHotel';
+import useTicket from '../../../hooks/api/useTicket';
+
 export default function Hotel() {
-  return 'Hotel: Em breve!';
+  const [includesHotel, setIncludesHotel] = useState(undefined);
+  const { getticket } = useTicket();
+
+  useEffect(async() => {
+    const ticket = await getticket();
+
+    const resincludesHotel = ticket.TicketType.includesHotel;
+
+    setIncludesHotel(resincludesHotel);
+  }, []);
+  return <>{!includesHotel ? <NotIncludesHotel /> : ''}</>;
 }
