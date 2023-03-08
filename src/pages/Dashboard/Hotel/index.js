24d0480f2d5718b2c00a9e763670fa432c1efacc
ConstantinoRafael <rafael.constantino89@gmail.com>
2023-03-08 05:39:39 -0300

    feat: paymente required page

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,18 +1,34 @@
 import { useState } from 'react';
 import { useEffect } from 'react';
 import NotIncludesHotel from '../../../components/Dashboard/NotIncludesHotel';
+import PaymenteRequired from '../../../components/Dashboard/PaymentRequired';
 import useTicket from '../../../hooks/api/useTicket';
 
 export default function Hotel() {
   const [includesHotel, setIncludesHotel] = useState(undefined);
   const { getticket } = useTicket();
+  const [paymentRequired, setPaymentRequired] = useState(undefined);
 
   useEffect(async() => {
     const ticket = await getticket();
 
-    const resincludesHotel = ticket.TicketType.includesHotel;
+    const resIncludesHotel = ticket.TicketType.includesHotel;
+    const resStatus = ticket.status;
 
-    setIncludesHotel(resincludesHotel);
+    setIncludesHotel(resIncludesHotel);
+
+    if (resIncludesHotel === true && resStatus !== 'PAID') {
+      setPaymentRequired(true);
+    }
   }, []);
-  return <>{!includesHotel ? <NotIncludesHotel /> : ''}</>;
+
+  if (!includesHotel) {
+    return <NotIncludesHotel />;
+  }
+
+  if (paymentRequired) {
+    return <PaymenteRequired/>;
+  }
+
+  return <>Escolher Hotelzinho s2</>;
 }
