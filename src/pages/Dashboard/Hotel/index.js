9c12ed19cfd2711446747ded947da704c330a69e
Mateus Borges <borgesm.work@gmail.com>
2023-03-10 14:53:11 -0300

    feat: remove mock data and display based on data from API

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -2,13 +2,17 @@ import NotIncludesHotel from '../../../components/Dashboard/NotIncludesHotel';
 import PaymenteRequired from '../../../components/Dashboard/PaymentRequired';
 import styled from 'styled-components';
 import { useEffect } from 'react';
+import useHotels from '../../../hooks/api/useHotels';
 import { useState } from 'react';
 import useTicket from '../../../hooks/api/useTicket';
 
 export default function Hotel() {
   const [includesHotel, setIncludesHotel] = useState(undefined);
   const { getticket } = useTicket();
+  const { getHotel } = useHotels();
   const [paymentRequired, setPaymentRequired] = useState(undefined);
+  const [hotels, setHotels] = useState([]);
+  const [accommodation, setAccommodation] = useState({ hotelId: '', roomId: '' });
 
   // eslint-disable-next-line space-before-function-paren
   useEffect(async () => {
@@ -21,8 +25,25 @@ export default function Hotel() {
 
     if (resIncludesHotel === true && resStatus !== 'PAID') {
       setPaymentRequired(true);
+    } else {
+      const hotelList = await getHotel();
+
+      if (!hotelList) return;
+
+      const hotelsWithTotalCapacity = hotelList.map((hotel) => ({
+        id: hotel.id,
+        name: hotel.name,
+        image: hotel.image,
+        totalCapacity: hotel.Rooms.reduce((total, room) => total + room.capacity, 0),
+        singles: hotel.Rooms.some((room) => room.capacity === 1),
+        doubles: hotel.Rooms.some((room) => room.capacity === 2),
+        triples: hotel.Rooms.some((room) => room.capacity === 3),
+        rooms: hotel.Rooms,
+      }));
+
+      setHotels(hotelsWithTotalCapacity);
     }
-  }, []);
+  }, [accommodation]);
 
   if (!includesHotel) {
     return <NotIncludesHotel />;
@@ -37,18 +58,29 @@ export default function Hotel() {
       <h1>Escolha de hotel e quarto</h1>
       <h2>Primeiro escolha seu hotel</h2>
       <Hotels>
-        <button>
-          <img
-            src="https://www.hotelpremiumcampinas.com.br/wp-content/uploads/2021/08/fachada-scaled.jpg"
-            alt="HotelImg"
-          />
-          <h3>Driven Resort</h3>
-          <h4>Tipos de acomodação:</h4>
-          <p>Single e Double</p>
-          <h4>Vagas disponíveis:</h4>
-          <p>103</p>
-        </button>
-        <button></button>
+        {hotels.map((hotel) => {
+          return (
+            <SingleHotel
+              key={hotel.id}
+              id={hotel.id}
+              onClick={() => setAccommodation({ ...accommodation, hotelId: hotel.id })}
+              clicked={accommodation.hotelId === hotel.id ? 'true' : 'false'}
+            >
+              <img src={hotel.image} alt="HotelImg" />
+              <h3>{hotel.name}</h3>
+              <h4>Tipos de acomodação:</h4>
+              <p>
+                {hotel.singles && 'Singles'}
+                {hotel.singles && (hotel.doubles || hotel.triples) && ' | '}
+                {hotel.doubles && 'Doubles'}
+                {hotel.doubles && hotel.triples && ' | '}
+                {hotel.triples && 'Triples'}
+              </p>
+              <h4>Vagas disponíveis:</h4>
+              <p>{hotel.totalCapacity}</p>
+            </SingleHotel>
+          );
+        })}
       </Hotels>
     </HotelContainer>
   );
@@ -69,42 +101,43 @@ const HotelContainer = styled.div`
 
 const Hotels = styled.div`
   display: flex;
-  button {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    width: 196px;
-    height: 264px;
-    padding: 15px;
-    margin: 15px 20px 0 0;
-    border: ${(props) => (props.clicked ? '' : '1px solid #cecece')};
-    border-radius: 10px;
-    cursor: pointer;
-    background-color: ${(props) => (props.clicked ? '#feeed2' : '#ebebeb')};
+`;
 
-    img {
-      width: 168px;
-      height: 109px;
-      border-radius: 5px;
-      object-fit: contain;
-      margin: 0 auto 10px auto;
-    }
+const SingleHotel = styled.button`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 196px;
+  height: 264px;
+  padding: 15px;
+  margin: 15px 20px 0 0;
+  border: ${(props) => (props.clicked === 'true' ? '' : '1px solid #cecece')};
+  border-radius: 10px;
+  cursor: pointer;
+  background-color: ${(props) => (props.clicked === 'true' ? '#feeed2' : '#ebebeb')};
 
-    h3 {
-      font-size: 20px;
-      color: #343434;
-    }
+  img {
+    width: 168px;
+    height: 109px;
+    border-radius: 5px;
+    object-fit: cover;
+    margin: 0 auto 10px auto;
+  }
 
-    h4,
-    p {
-      font-size: 12px;
-      color: #3c3c3c;
-      line-height: 16px;
-    }
+  h3 {
+    font-size: 20px;
+    color: #343434;
+  }
 
-    h4 {
-      margin-top: 10px;
-      font-weight: 700;
-    }
+  h4,
+  p {
+    font-size: 12px;
+    color: #3c3c3c;
+    line-height: 16px;
+  }
+
+  h4 {
+    margin-top: 10px;
+    font-weight: 700;
   }
 `;
