60e71d571ebb20a8fc924121d26cc35160b3c1d7
Luiz-Cunha-Dev <luizcunha.dev99@gmail.com>
2023-03-15 11:38:16 -0300

    feat: warning screens to the activities page

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,37 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import NotIncludesHotelActivities from '../../../components/Dashboard/NotIncludesHotel/activities';
+import PaymenteRequiredActivities from '../../../components/Dashboard/PaymentRequired/activities';
+import useTicket from '../../../hooks/api/useTicket';
+
 export default function Activities() {
+  const [includesHotel, setIncludesHotel] = useState(undefined);
+  const { getticket } = useTicket();
+  const [paymentRequired, setPaymentRequired] = useState(undefined);
+
+  useEffect(async() => {
+    try {
+      const ticket = await getticket();
+      const resIncludesHotel = ticket.TicketType.includesHotel;
+      const resStatus = ticket.status;
+
+      setIncludesHotel(resIncludesHotel);
+
+      if (resStatus !== 'PAID') {
+        setPaymentRequired(true);
+      }
+    } catch {
+      setPaymentRequired(true);
+    }
+  }, []);
+
+  if (paymentRequired) {
+    return <PaymenteRequiredActivities />;
+  }
+
+  if (!includesHotel) {
+    return <NotIncludesHotelActivities />;
+  }
+
   return 'Atividades: Em breve!';
 }
