9592abdf7ede7bd926d2fb2078fa56dff906cfe4
Mateus Borges <borgesm.work@gmail.com>
2023-03-19 10:13:59 -0300

    style: formatter

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,19 +1,20 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
-import styled from 'styled-components';
-import NotIncludesHotelActivities from '../../../components/Dashboard/NotIncludesHotel/activities';
-import PaymenteRequiredActivities from '../../../components/Dashboard/PaymentRequired/activities';
-import useTicket from '../../../hooks/api/useTicket';
-import Typography from '@material-ui/core/Typography';
 import Day from '../../../components/activities/day';
 import Location from '../../../components/activities/location';
+import NotIncludesHotelActivities from '../../../components/Dashboard/NotIncludesHotel/activities';
+import PaymenteRequiredActivities from '../../../components/Dashboard/PaymentRequired/activities';
+import Typography from '@material-ui/core/Typography';
+import styled from 'styled-components';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import useTicket from '../../../hooks/api/useTicket';
 
 export default function Activities() {
   const [includesHotel, setIncludesHotel] = useState(undefined);
   const { getticket } = useTicket();
   const [paymentRequired, setPaymentRequired] = useState(undefined);
 
-  useEffect(async() => {
+  // eslint-disable-next-line space-before-function-paren
+  useEffect(async () => {
     try {
       const ticket = await getticket();
       const resIncludesHotel = ticket.TicketType.includesHotel;
